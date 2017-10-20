322b45c6ecf48baa89190f37b35bd79a6b16db8b
dew01ke <me@andrey-volkov.ru>
2017-10-20 23:25:37 +0300

    Авторизация в ВК (в памяти)

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,3 +1,44 @@
 (function() {
     console.log('[log]: popup script init');
+
+    function sendBackgroundRequest(method, data, callback) {
+        var request = {};
+        if (method) request.method = method;
+        if (data) request.data = data;
+
+        chrome.extension.sendMessage(request,
+            function(response) {
+                if (callback) { callback(response); }
+            }
+        );
+    }
+
+    var cache = {
+        button_auth_wrapper: $('.button-auth-wrapper'),
+        button_auth: $('.button-auth'),
+        button_control_wrapper: $('.button-control-wrapper'),
+        button_control: $('.button-control')
+    };
+
+    cache.button_auth.click(function(e) {
+        cache.button_auth_wrapper.hide();
+
+        sendBackgroundRequest('auth_user', null, function(data) {
+            console.log('auth_user', data);
+        });
+    });
+
+    function onInit() {
+        sendBackgroundRequest('get_auth_state', null, function(data) {
+            if (data && data.user_id) {
+                cache.button_auth_wrapper.hide();
+                cache.button_control_wrapper.show();
+            } else {
+                cache.button_auth_wrapper.show();
+                cache.button_control_wrapper.hide();
+            }
+        });
+    }
+
+    onInit();
 })();
